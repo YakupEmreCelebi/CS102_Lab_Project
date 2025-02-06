05847ee48aa1cdc0b7f4ea62423d0f02a82ebc86
devyec <ykpmrclb06@gmail.com>
2025-02-06 17:03:42 +0300

    c part of Assignment_2_Collaboration.java done (Maximum and minimum methods)

--- Assignment_2_Collaboration.java
+++ Assignment_2_Collaboration.java
@@ -1,6 +1,29 @@
 import java.util.Random;
 
 public class Assignment_2_Collaboration {
+     public static int findMinimumOfArray(int [] intArray){
+        int minimum = intArray[0];
+        for(int i=0; i<100; i++)
+        {
+            if(intArray[i] <= minimum)
+            {
+                minimum = intArray[i];
+            }
+        }
+        return minimum;
+    }
+
+    public static int findMaximumOfArray(int [] intArray){
+        int maximum = intArray[0];
+        for(int i=0; i<100; i++)
+        {
+            if(intArray[i] >= maximum)
+            {
+                maximum = intArray[i];
+            }
+        }
+        return maximum;
+    }
     public static void main(String[] args) {
         int array_size = 100;
         int[] numbers = new int[array_size];
