d80dc3de8c44af1290043563b230a948b4dca3ef
EnesAkbulut2230 <enes.akblt382@gmail.com>
2025-02-07 18:40:49 +0300

    Added menu options

--- Assignment_2_Collaboration.java
+++ Assignment_2_Collaboration.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Assignment_2_Collaboration {
      public static int findMinimumOfArray(int [] intArray){
@@ -25,6 +26,8 @@ public class Assignment_2_Collaboration {
         return maximum;
     }
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
         int array_size = 100;
         int[] numbers = new int[array_size];
 
@@ -38,5 +41,26 @@ public class Assignment_2_Collaboration {
             int result = r.nextInt(high - low) + low;
             numbers[i] = result;
         }
+
+        do {
+            System.out.println();
+            System.out.println(
+                    "    Choose the operation number! \n \n1. Find the minimum and maximum values in array. \n2. Display how each element of the array differs from the average. \n3. Find the sum of elements with odd- and even- numberede indexes. \n0. Exit.");
+            System.out.print("Enter a number: ");
+            int operationNumber = scanner.nextInt();
+            System.out.println();
+            if (operationNumber == 1) {
+                System.out.println("Maximum value in the array is " + findMaximumOfArray(numbers));
+                System.out.println("Minimum value in the array is " + findMinimumOfArray(numbers));
+            } else if (operationNumber == 2) {
+                printDifferenceOfAvg(numbers);
+            } else if (operationNumber == 3) {
+                // System.out.println("Sum of odd numbers: "+ findTheSumOfOddNumbers(numbers));
+                // System.out.println("Sum of even numbers: "+ findTheSumOfEvenNumbers(numbers));
+            } else if (operationNumber == 0) {
+                scanner.close();
+                break;
+            }
+        } while (true);
     }
 }
